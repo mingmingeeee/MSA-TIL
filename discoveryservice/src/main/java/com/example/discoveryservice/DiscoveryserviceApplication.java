aaff288970f4328918fce96772b6d8c2287638c1
0613minjeong <0613minjeong@gmail.com>
2023-04-17 01:27:40 +0900

    feat: add apigateway, userservice, orderservice, catalogservice

--- discoveryservice/src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
+++ discoveryservice/src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer // Eureka 서버 활성화
 public class DiscoveryserviceApplication {
 
     public static void main(String[] args) {
